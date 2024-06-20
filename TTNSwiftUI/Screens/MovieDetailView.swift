2571e2b94fb48b6a479a329f7b55175a8a520718
Gaurav pandey <gaurav.pandey1@tothenew.com>
2024-06-20 10:26:29 +0530

    Initial commit

---
+++ TTNSwiftUI/Screens/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  TTNSwiftUI
+//
+//  Created by Gaurav Pandey on 20/06/24.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
